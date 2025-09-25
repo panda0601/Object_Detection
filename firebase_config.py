b3d37c6666009c7d6f277c04da7ef5560fd96cc0
keerthana U <134399207+panda0601@users.noreply.github.com>
2025-09-25 22:42:12 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import streamlit as st
+import json
 
-cred = credentials.Certificate("/Users/keerthanaulaganathan/Desktop/python/ML/OpenCV/stock_env/firebase.json")
-firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://stockleveldetection-default-rtdb.firebaseio.com/"
-})
+# Load Firebase credentials from secrets
+firebase_creds = st.secrets["firebase"]
+cred = credentials.Certificate(dict(firebase_creds))
+
+# Initialize Firebase app
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": "https://stockleveldetection-default-rtdb.firebaseio.com/"
+    })
 
 def get_db():
     return db.reference("stock_logs")
