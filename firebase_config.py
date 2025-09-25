bedbdbc248d4d881f3cd2c8bf3993a40a865c959
keerthana U <134399207+panda0601@users.noreply.github.com>
2025-09-25 22:26:04 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("firebase.json")
+cred = credentials.Certificate("/Users/keerthanaulaganathan/Desktop/python/ML/OpenCV/stock_env/firebase.json")
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://stockleveldetection-default-rtdb.firebaseio.com/"
 })
